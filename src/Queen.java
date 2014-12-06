d7eaf6f0b34df6ff777e2b9553ffba877378f90c
sonicxml <sonicxml@gmail.com>
2014-12-06 14:55:59 -0500

    Queen: code cleanup

--- src/Queen.java
+++ src/Queen.java
@@ -37,23 +37,25 @@ public class Queen implements ChessPiece {
             for (int j = -1; j < 2; j++) {
                 int scale = 1;
                 try {
-                    while (chessPieces[x + i*scale][y + j*scale] == null) {
-                        move = new Coords(x + i*scale, y + j*scale);
+                    int p = x + i * scale;
+                    int q = y + j * scale;
+                    while (chessPieces[p][q] == null) {
+                        move = new Coords(p, q);
                         possibleMoves.add(move);
                         scale++;
+                        p = x + i * scale;
+                        q = y + j * scale;
                     }
-                    if (chessPieces[x + i*scale][y + j*scale] != null &&
-                        chessPieces[x + i*scale][y + j*scale].isWhite() != isWhite) {
-                        move = new Coords(x + i*scale, y + j*scale);
+                    if (chessPieces[p][q] != null &&
+                            chessPieces[p][q].isWhite() != isWhite) {
+                        move = new Coords(p, q);
                         possibleMoves.add(move);
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
                     // Do Nothing
                 }
-
             }
         }
-
         return possibleMoves;
     }
 
